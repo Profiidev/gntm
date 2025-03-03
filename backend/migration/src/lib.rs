9a6cf177da460dc8637ad0c26cea4ad18f1b623f
GitHub Action <action@github.com>
2025-03-03 17:23:41 +0000

    Fix code style issues with rustfmt

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -13,12 +13,12 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
   fn migrations() -> Vec<Box<dyn MigrationTrait>> {
     vec![
-            Box::new(m20250301_204945_create_season_table::Migration),
-            Box::new(m20250301_205124_create_candidate_table::Migration),
-            Box::new(m20250301_210020_create_user_table::Migration),
-            Box::new(m20250301_210029_create_bingo_table::Migration),
-            Box::new(m20250301_215149_create_key_table::Migration),
-            Box::new(m20250301_215333_create_invalid_jwt_table::Migration),
-        ]
+      Box::new(m20250301_204945_create_season_table::Migration),
+      Box::new(m20250301_205124_create_candidate_table::Migration),
+      Box::new(m20250301_210020_create_user_table::Migration),
+      Box::new(m20250301_210029_create_bingo_table::Migration),
+      Box::new(m20250301_215149_create_key_table::Migration),
+      Box::new(m20250301_215333_create_invalid_jwt_table::Migration),
+    ]
   }
 }
